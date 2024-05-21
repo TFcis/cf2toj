320f21bb4696b0373b00a101d2663da645640f50
tobiichi3227 <cz1346219@gmail.com>
2024-05-21 09:06:27 +0800

    feat: wait compress to complete

--- README.md
+++ README.md
@@ -3,6 +3,4 @@
 
 ## 使用方法
 1. `python cf2toj.py /path/to/tps/directory /path/to/output/directory`
-
-2. ## 注意事項
-本程式不會等待壓縮處理完就先結束，`.tar.xz`檔案大小不再增加時才確實壓縮完成。
+2. 等待壓縮完成
--- cf2toj.py
+++ cf2toj.py
@@ -1,111 +1,121 @@
+import asyncio
 import argparse
 import json
 import logging
 import os
 import subprocess
 import xml.etree.ElementTree as ET
-from function import makedirs, copyfile
+from function import makedirs, copyfile, run_and_wait_process
 
-args_parser = argparse.ArgumentParser(description='cf2toj')
-args_parser.add_argument('inputpath', type=str, help='輸入資料夾')
-args_parser.add_argument('outputpath', type=str, help='輸出資料夾')
-args_parser.add_argument('-d', '--debug', action='store_const', dest='loglevel', const=logging.DEBUG)
-args_parser.set_defaults(loglevel=logging.INFO)
-args = args_parser.parse_args()
-inputpath = args.inputpath
-outputpath = args.outputpath
+async def main():
+    args_parser = argparse.ArgumentParser(description='cf2toj')
+    args_parser.add_argument('inputpath', type=str, help='輸入資料夾')
+    args_parser.add_argument('outputpath', type=str, help='輸出資料夾')
+    args_parser.add_argument('-d', '--debug', action='store_const', dest='loglevel', const=logging.DEBUG)
+    args_parser.set_defaults(loglevel=logging.INFO)
+    args = args_parser.parse_args()
+    inputpath = args.inputpath
+    outputpath = args.outputpath
 
-logging.basicConfig(level=args.loglevel, format='%(asctime)s %(levelname)s %(message)s')
+    logging.basicConfig(level=args.loglevel, format='%(asctime)s %(levelname)s %(message)s')
 
-with open(os.path.join(inputpath, 'problem.xml')) as xml_f:
-    xml = ET.parse(xml_f)
-    root = xml.getroot()
+    with open(os.path.join(inputpath, 'problem.xml')) as xml_f:
+        xml = ET.parse(xml_f)
+        root = xml.getroot()
 
-conf = {
-    'timelimit': 0,
-    'memlimit': 0,
-    'compile': 'g++',
-    'score': 'rate',
-    'check': 'diff',
-    'test': [],
-    'metadata': {}
-}
+    conf = {
+        'timelimit': 0,
+        'memlimit': 0,
+        'compile': 'g++',
+        'score': 'rate',
+        'check': 'diff',
+        'test': [],
+        'metadata': {}
+    }
 
-conf['timelimit'] = root.find('./judging/testset/time-limit').text
-conf['memlimit'] = int(int(root.find('./judging/testset/memory-limit').text) / 1024)
+    conf['timelimit'] = root.find('./judging/testset/time-limit').text
+    conf['memlimit'] = int(int(root.find('./judging/testset/memory-limit').text) / 1024)
 
-makedirs(outputpath, 'res/testdata')
+    makedirs(outputpath, 'res/testdata')
 
-tasks_group = {}
-cnt = 1
-is_one_group = True
-for test in root.findall("./judging/testset/tests/"):
-    g = test.attrib.get('group', 0)
-    g_weight = test.attrib.get('points', 0)
-    if g != 0:
-        is_one_group = False
+    tasks_group = {}
+    cnt = 1
+    is_one_group = True
+    for test in root.findall("./judging/testset/tests/"):
+        g = test.attrib.get('group', 0)
+        g_weight = test.attrib.get('points', 0)
+        if g != 0:
+            is_one_group = False
 
-    if g not in tasks_group:
-        tasks_group[g] = {
-            'weight': int(float(g_weight)),
-            'data': [cnt]
-        }
-    else:
-        tasks_group[g]['weight'] += int(float(g_weight))
-        tasks_group[g]['data'].append(cnt)
+        if g not in tasks_group:
+            tasks_group[g] = {
+                'weight': int(float(g_weight)),
+                'data': [cnt]
+            }
+        else:
+            tasks_group[g]['weight'] += int(float(g_weight))
+            tasks_group[g]['data'].append(cnt)
 
-    cnt += 1
+        cnt += 1
 
-if is_one_group:
-    tasks_group[0]['weight'] = 100
+    if is_one_group:
+        tasks_group[0]['weight'] = 100
 
 # format_str = "{:0" + str(len(str(cnt)) + 1) + "}"
-format_str = "{:02}"
+    format_str = "{:02}"
 
-for g in tasks_group.values():
-    conf['test'].append(g)
+    for g in tasks_group.values():
+        conf['test'].append(g)
 
-    for i in g['data']:
-        copyfile(
-            (inputpath, 'tests', (format_str + "").format(i)),
-            (outputpath, 'res/testdata', "{}.in".format(i)),
-        )
-        copyfile(
-            (inputpath, 'tests', (format_str + ".a").format(i)),
-            (outputpath, 'res/testdata', "{}.out".format(i)),
-        )
+        for i in g['data']:
+            copyfile(
+                (inputpath, 'tests', (format_str + "").format(i)),
+                (outputpath, 'res/testdata', "{}.in".format(i)),
+            )
+            copyfile(
+                (inputpath, 'tests', (format_str + ".a").format(i)),
+                (outputpath, 'res/testdata', "{}.out".format(i)),
+            )
 
-logging.info('Creating config file')
-with open(os.path.join(outputpath, 'conf.json'), 'w') as conffile:
-    json.dump(conf, conffile, indent=4)
+    logging.info('Creating config file')
+    with open(os.path.join(outputpath, 'conf.json'), 'w') as conffile:
+        json.dump(conf, conffile, indent=4)
 
-makedirs(outputpath, 'http')
-logging.info('Copying statements')
-statement_path = os.path.join(inputpath, 'statements/.html/chinese')
-if not os.path.exists(statement_path):
-    statement_path = os.path.join(inputpath, 'statements/.html/english')
+    makedirs(outputpath, 'http')
+    logging.info('Copying statements')
+    statement_path = os.path.join(inputpath, 'statements/.html/chinese')
+    if not os.path.exists(statement_path):
+        statement_path = os.path.join(inputpath, 'statements/.html/english')
 
-for filepath in os.listdir(statement_path):
-    if filepath == "problem.html":
-        copyfile(
-            (statement_path, 'problem.html'),
-            (outputpath, 'http', 'cont.html')
-        )
+    for filepath in os.listdir(statement_path):
+        if filepath == "problem.html":
+            copyfile(
+                (statement_path, 'problem.html'),
+                (outputpath, 'http', 'cont.html')
+            )
+        else:
+            copyfile(
+                (statement_path, filepath),
+                (outputpath, 'http', filepath),
+            )
+
+    subprocess.run(['sed', '-i', "s/background-color: #efefef;//", f'{outputpath}/http/problem-statement.css'])
+
+    logging.info('Compress starting')
+    returncode = await run_and_wait_process('tar', *[
+        '-C',
+        outputpath,
+        '-cJf',
+        os.path.join(os.path.dirname(outputpath), '{}.tar.xz'.format(os.path.basename(outputpath))),
+        'http',
+        'res',
+        'conf.json',
+    ])
+
+    if returncode != 0:
+        logging.info('Compress failed')
     else:
-        copyfile(
-            (statement_path, filepath),
-            (outputpath, 'http', filepath),
-        )
+        logging.info('Compress successfully')
 
-subprocess.run(['sed', '-i', "s/background-color: #efefef;//", f'{outputpath}/http/problem-statement.css'])
-
-p = subprocess.Popen([
-    'tar',
-    '-C',
-    outputpath,
-    '-cJf',
-    os.path.join(os.path.dirname(outputpath), '{}.tar.xz'.format(os.path.basename(outputpath))),
-    'http',
-    'res',
-    'conf.json'
-])
+if __name__ == '__main__':
+    asyncio.run(main())
