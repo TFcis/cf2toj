2a6c331f8bdf2ced66945769dad8ffe3cafbab15
blameazu <85005499+blameazu@users.noreply.github.com>
2024-08-24 13:54:33 +0800

    clear_ouput_directory option update (#3)
    If the option `-c (--clear_output_directory)` is used, the program will generate a directory with a random name for temporary file usage.
    Once the compression process is completed by the program, the temporary directory will be deleted.

--- cf2toj.py
+++ cf2toj.py
@@ -6,8 +6,13 @@ import os
 import subprocess
 import xml.etree.ElementTree as ET
 import collections
+import random
+import string
 
 from function import makedirs, copyfile, rmdir, run_and_wait_process
+
+def generate_random_file_name(length=8):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
 
 def replace_in_file(file_path, search_text, replace_text):
     try:
@@ -42,7 +47,13 @@ async def main():
     outputpath = args.outputpath
 
     logging.basicConfig(level=args.loglevel, format='%(asctime)s %(levelname)s %(message)s')
-
+    
+    if args.is_clear_output_dir:
+        random_dir = generate_random_file_name()
+        tmp_outputpath = os.path.join(os.path.dirname(outputpath), random_dir)
+    else:
+        tmp_outputpath = outputpath
+        
     with open(os.path.join(inputpath, 'problem.xml'), encoding='utf-8') as xml_f:
         xml = ET.parse(xml_f)
         root = xml.getroot()
@@ -52,7 +63,7 @@ async def main():
         'memlimit': 0,
         'compile': 'g++',
         'score': 'rate',
-        'check': 'diff',
+        'check': 'diff', # two mode : 'diff' or 'diff-strict'
         'test': [],
         'metadata': {}
     }
@@ -60,7 +71,7 @@ async def main():
     conf['timelimit'] = root.find('./judging/testset/time-limit').text
     conf['memlimit'] = int(int(root.find('./judging/testset/memory-limit').text) / 1024)
 
-    makedirs(outputpath, 'res/testdata')
+    makedirs(tmp_outputpath, 'res/testdata')
 
     tasks_group = collections.OrderedDict()
     groups_enabled = root.find('./judging/testset/groups/') is not None
@@ -95,12 +106,12 @@ async def main():
         for src in g['remap']:
             copyfile(
                 (inputpath, 'tests', format_str.format(src)),
-                (outputpath, 'res/testdata', "{}.in".format(dst)),
+                (tmp_outputpath, 'res/testdata', "{}.in".format(dst)),
             )
 
             copyfile(
                 (inputpath, 'tests', format_str.format(src) + ".a"),
-                (outputpath, 'res/testdata', "{}.out".format(dst)),
+                (tmp_outputpath, 'res/testdata', "{}.out".format(dst)),
             )
             g['data'].append(dst)
             dst += 1
@@ -109,10 +120,10 @@ async def main():
         conf['test'].append(g)
 
     logging.info('Creating config file')
-    with open(os.path.join(outputpath, 'conf.json'), 'w', encoding='utf-8') as conffile:
+    with open(os.path.join(tmp_outputpath, 'conf.json'), 'w', encoding='utf-8') as conffile:
         json.dump(conf, conffile, indent=4, ensure_ascii=False)
 
-    makedirs(outputpath, 'http')
+    makedirs(tmp_outputpath, 'http')
     logging.info('Copying statements')
 
     statement_path = ''
@@ -129,19 +140,19 @@ async def main():
                 if filepath == "problem.html":
                     copyfile(
                         (statement_path, 'problem.html'),
-                        (outputpath, 'http', 'cont.html')
+                        (tmp_outputpath, 'http', 'cont.html')
                     )
                 else:
                     copyfile(
                         (statement_path, filepath),
-                        (outputpath, 'http', filepath),
+                        (tmp_outputpath, 'http', filepath),
                     )
             
             # check
             if check_sed():
-                subprocess.run(['sed', '-i', 's/background-color: #efefef;//g', f'{outputpath}/http/problem-statement.css'])
+                subprocess.run(['sed', '-i', 's/background-color: #efefef;//g', f'{tmp_outputpath}/http/problem-statement.css'])
             else:
-                replace_in_file(f'{outputpath}/http/problem-statement.css', "background-color: #efefef;", "")
+                replace_in_file(f'{tmp_outputpath}/http/problem-statement.css', "background-color: #efefef;", "")
         else:
             logging.warning('No statement found at path: {}'.format(statement_path))
     else:
@@ -150,9 +161,9 @@ async def main():
     logging.info('Compress starting')
     returncode = await run_and_wait_process('tar', *[
         '-C',
-        outputpath,
+        tmp_outputpath,
         '-cJf',
-        os.path.join(os.path.dirname(outputpath), '{}.tar.xz'.format(os.path.basename(outputpath))),
+        os.path.join(os.path.dirname(tmp_outputpath), '{}.tar.xz'.format(os.path.basename(tmp_outputpath))),
         'http',
         'res',
         'conf.json',
@@ -165,7 +176,7 @@ async def main():
 
     if args.is_clear_output_dir:
         logging.info('Deleting output directory')
-        rmdir(outputpath)
+        rmdir(tmp_outputpath)
 
 if __name__ == '__main__':
     asyncio.run(main())
